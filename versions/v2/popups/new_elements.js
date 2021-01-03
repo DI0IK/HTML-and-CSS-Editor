39666867675e8b47ddd52ceed7df8f5830345405
DI0IK <stahl.dominik@online.de>
2021-01-03 19:23:04 +0100

    fix

--- versions/v2/popups/new_elements.js
+++ versions/v2/popups/new_elements.js
@@ -1,7 +1,9 @@
 var input_id;
 var input_class;
+var input_text;
 var text_id;
 var text_class;
+var text_text;
 var btn_apply;
 var type_list;
 var type_other;
@@ -9,8 +11,10 @@ var type_other;
 function onload() {
   input_id = document.getElementById("input_id");
   input_class = document.getElementById("input_class");
+  input_text = document.getElementById("input_text");
   text_id = document.getElementById("text_id");
   text_class = document.getElementById("text_class");
+  text_text = document.getElementById("text_text");
   btn_apply = document.getElementById("btn_apply");
   type_list = document.getElementById("type");
   type_other = document.getElementById("type_other");
@@ -21,115 +25,94 @@ function type_change() {
   if (type_list.value == "-") {
     text_id.hidden = true;
     text_class.hidden = true;
+    text_text.hidden = true;
     type_other.hidden = true;
-    console.log(type + " disabled");
   }
   if (type_list.value == "p") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = false;
     type_other.hidden = true;
-    console.log(type + " enabled");
   }
-  if (type_list.value == "h1") {
+  if (
+    type_list.value == "h1" ||
+    type_list.value == "h2" ||
+    type_list.value == "h3" ||
+    type_list.value == "h4" ||
+    type_list.value == "h5" ||
+    type_list.value == "h6"
+  ) {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = false;
     type_other.hidden = true;
-    console.log(type + " enabled");
-  }
-  if (type_list.value == "h2") {
-    text_id.hidden = false;
-    text_class.hidden = false;
-    type_other.hidden = true;
-    console.log(type + " enabled");
-  }
-  if (type_list.value == "h3") {
-    text_id.hidden = false;
-    text_class.hidden = false;
-    type_other.hidden = true;
-    console.log(type + " enabled");
-  }
-  if (type_list.value == "h4") {
-    text_id.hidden = false;
-    text_class.hidden = false;
-    type_other.hidden = true;
-    console.log(type + " enabled");
-  }
-  if (type_list.value == "h5") {
-    text_id.hidden = false;
-    text_class.hidden = false;
-    type_other.hidden = true;
-    console.log(type + " enabled");
-  }
-  if (type_list.value == "h6") {
-    text_id.hidden = false;
-    text_class.hidden = false;
-    type_other.hidden = true;
-    console.log(type + " enabled");
   }
   if (type_list.value == "ul") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = true;
     type_other.hidden = true;
-    console.log(type + " enabled");
   }
   if (type_list.value == "ol") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = true;
     type_other.hidden = true;
-    console.log(type + " enabled");
   }
   if (type_list.value == "li") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = false;
     type_other.hidden = true;
-    console.log(type + " enabled");
   }
   if (type_list.value == "a") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = true;
     type_other.hidden = true;
-    console.log(type + " enabled");
   }
   if (type_list.value == "img") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = true;
     type_other.hidden = true;
-    console.log(type + " enabled");
   }
   if (type_list.value == "iframe") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = true;
     type_other.hidden = true;
-    console.log(type + " enabled");
   }
   if (type_list.value == "button") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = false;
     type_other.hidden = true;
-    console.log(type + " enabled");
   }
   if (type_list.value == "select") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = true;
     type_other.hidden = true;
-    console.log(type + " enabled");
   }
   if (type_list.value == "option") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = false;
     type_other.hidden = true;
-    console.log(type + " enabled");
   }
   if (type_list.value == "textarea") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = true;
     type_other.hidden = true;
-    console.log(type + " enabled");
   }
   if (type_list.value == "other") {
     text_id.hidden = false;
     text_class.hidden = false;
+    text_text.hidden = true;
     type_other.hidden = false;
-    console.log(type + " enabled");
   }
 }
+
+function apply() {}
