4c90e1d6bd5ca75298639c620141114ae0923679
DI0IK <stahl.dominik@online.de>
2021-01-04 17:32:07 +0100

    added some elements

--- versions/v2/popups/new_elements.js
+++ versions/v2/popups/new_elements.js
@@ -1,9 +1,13 @@
 var input_id;
 var input_class;
 var input_text;
+var input_src;
+var input_href;
 var text_id;
 var text_class;
 var text_text;
+var text_src;
+var text_href;
 var btn_apply;
 var type_list;
 var type_other;
@@ -12,9 +16,13 @@ function onload() {
   input_id = document.getElementById("input_id");
   input_class = document.getElementById("input_class");
   input_text = document.getElementById("input_text");
+  input_src = document.getElementById("input_src");
+  input_href = document.getElementById("input_href");
   text_id = document.getElementById("text_id");
   text_class = document.getElementById("text_class");
   text_text = document.getElementById("text_text");
+  text_src = document.getElementById("text_src");
+  text_href = document.getElementById("text_href");
   btn_apply = document.getElementById("btn_apply");
   type_list = document.getElementById("type");
   type_other = document.getElementById("type_other");
@@ -26,12 +34,16 @@ function type_change() {
     text_id.hidden = true;
     text_class.hidden = true;
     text_text.hidden = true;
+    text_src.hidden = true;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (type_list.value == "p") {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = false;
+    text_src.hidden = true;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (
@@ -45,89 +57,130 @@ function type_change() {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = false;
+    text_src.hidden = true;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (type_list.value == "ul") {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = true;
+    text_src.hidden = true;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (type_list.value == "ol") {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = true;
+    text_src.hidden = true;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (type_list.value == "li") {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = false;
+    text_src.hidden = true;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (type_list.value == "a") {
     text_id.hidden = false;
     text_class.hidden = false;
-    text_text.hidden = true;
+    text_text.hidden = false;
+    text_src.hidden = true;
+    text_href.hidden = false;
     type_other.hidden = true;
   }
   if (type_list.value == "img") {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = true;
+    text_src.hidden = false;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (type_list.value == "iframe") {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = true;
+    text_src.hidden = false;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (type_list.value == "button") {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = false;
+    text_src.hidden = true;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (type_list.value == "select") {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = true;
+    text_src.hidden = true;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (type_list.value == "option") {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = false;
+    text_src.hidden = true;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (type_list.value == "textarea") {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = true;
+    text_src.hidden = true;
+    text_href.hidden = true;
     type_other.hidden = true;
   }
   if (type_list.value == "other") {
     text_id.hidden = false;
     text_class.hidden = false;
     text_text.hidden = true;
+    text_src.hidden = true;
+    text_href.hidden = true;
     type_other.hidden = false;
   }
+  change_text();
 }
 
 function apply() {
+  var open_tag = "";
+  var value = "";
+  var close_tag = "";
+
+  open_tag += "<" + type.value;
+  if (input_id.value != "") open_tag += ` id= "` + input_id.value + `"`;
+  if (input_class.value != "")
+    open_tag += ` class= "` + input_class.value + `"`;
+  if (input_src.value != "") open_tag += ` src= "` + input_src.value + `"`;
+  if (input_href.value != "") open_tag += ` href= "` + input_href.value + `"`;
+  open_tag += ">";
+
+  if (input_text.value != "") value += input_text.value;
+
+  if (type.value != "img") {
+    close_tag += "</" + type.value;
+    close_tag += ">";
+  }
+
   var o_html = localStorage.getItem("html-code");
   localStorage.setItem(
     "html-code",
     (
       o_html.split("</body>")[0] +
-      "\n<" +
-      type.value +
-      ">" +
-      input_text.value +
-      "</" +
-      type.value +
-      ">" +
+      "\n" +
+      open_tag +
+      value +
+      close_tag +
       "\n</body>"
     ).replace(/\n\s*\n/g, "\n")
   );
@@ -135,10 +188,34 @@ function apply() {
 }
 
 function change_text() {
-  console.log(input_text.value);
-  if (input_text.value != "") {
+  if (input_text.value != "" && text_text.hidden == false) {
     document.getElementById("btn_apply").disabled = false;
-  } else {
+  }
+  if (input_text.value == "" && text_text.hidden == false) {
+    document.getElementById("btn_apply").disabled = true;
+  }
+  if (input_src.value != "" && text_src.hidden == false) {
+    document.getElementById("btn_apply").disabled = false;
+  }
+  if (input_src.value == "" && text_src.hidden == false) {
+    document.getElementById("btn_apply").disabled = true;
+  }
+  if (
+    input_href.value != "" &&
+    input_text.value != "" &&
+    text_href.hidden == false &&
+    text_text.hidden == false &&
+    input_href.value.startsWith("https://") == true
+  ) {
+    document.getElementById("btn_apply").disabled = false;
+  }
+  if (
+    (input_href.value == "" ||
+      input_text.value == "" ||
+      input_href.value.startsWith("https://") == false) &&
+    text_href.hidden == false &&
+    text_text.hidden == false
+  ) {
     document.getElementById("btn_apply").disabled = true;
   }
 }
